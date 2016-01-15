6a04f26e26adad06bf0e2f64536ecde4553e6b43
varunmj <varunmj@users.noreply.github.com>
2016-01-16 01:29:40 +0530

    Create oddOReven.cpp

---
+++ oddOReven.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int b;
+cout<<"Enter the number:";
+cin>>b;
+if(b%2==0)
+{
+    cout<<b<<" is even number"<<endl;
+}else{
+    cout<<b<<" is odd number"<<endl;
+}
+return 0;
+}
