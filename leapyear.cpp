a96d901dcefdc91bd78f2fa576ac39ee4690341f
varunmj <varunmj@users.noreply.github.com>
2016-01-16 01:45:03 +0530

    Create leapyear.cpp

---
+++ leapyear.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a;
+cout<<"Enter the year:";
+cin>>a;
+if(a%4==0){
+    cout<<"it is a leap year";
+}else{
+    cout<<"it is not a leap year";
+}
+    return 0;
+}
