a61d6ba621a07f97a439a154e727d684cadff42f
varunmj <varunmj@users.noreply.github.com>
2016-01-16 01:30:30 +0530

    Create vowelORconsonant.cpp

---
+++ vowelORconsonant.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+char n;
+cin>>n;
+ if(n=='a'||n=='e'||n=='o'||n=='i'||n=='u') {
+    cout<<"vowel"<<endl;
+}
+    else{
+        cout<<"consonant"<<endl;
+    }
+    return 0;
+}
